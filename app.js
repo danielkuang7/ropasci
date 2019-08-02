9b78ae25132573ae99e0f09d9a5125ce5daac27b
Daniel Kuang <danielkuang7@github.com>
2019-08-02 10:04:38 -0400

    added animation

--- app.js
+++ app.js
@@ -34,19 +34,36 @@ const game = () => {
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll(".hands img");
+
+        hands.forEach(hand =>{
+            hand.addEventListener('animationend', function(){
+                this.style.animation = '';
+            });
+        });
+
         //Computer Options
         const computerOptions = ['rock', 'paper', 'scissors'];
 
         options.forEach(option => {
             option.addEventListener('click',function(){
-                //Computer Choice
-                const computerNumber = Math.floor(Math.random() * 3);
-                const computerChoice = computerOptions[computerNumber];
+            //Computer Choice
+            const computerNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[computerNumber];
+            
+            
+            setTimeout(()=>{
             //Here is where we call compare hands
             compareHands(this.textContent, computerChoice);
             //Update Images
             playerHand.src = `./assets/${this.textContent}.png`;
             computerHand.src = `./assets/${computerChoice}.png`;
+            }, 2000);
+
+            //Animations
+            playerHand.style.animation = "shakePlayer 2s ease";
+            computerHand.style.animation = "shakeComputer 2s ease";
+
             });
         });
     };
